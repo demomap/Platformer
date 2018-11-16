a1f87411f55029a7b911f55b84afd8f4816d59f8
Bryant_Jason <bryaj185@hsd.k12.or.us>
2018-11-16 11:20:02 -0800

    added player speed

--- Assets/Scripts/PlayerController1.cs
+++ Assets/Scripts/PlayerController1.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class PlayerController1 : MonoBehaviour {
     private Rigidbody m_rb;
+    public float speed = 10.0F;
+    public float max_speed = 4F;
 
 
 	// Use this for initialization
 	void Start () {
         m_rb = GetComponent<Rigidbody>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void FixedUpdate()
+    {
         float movement = Input.GetAxis("Horizontal");
-        m_rb.AddForce(new Vector3(movement, 0.0F, 0.0F));
-	}
+        m_rb.AddForce(new Vector3(movement * speed, 
+            0.0F, 0.0F));
+        m_rb.velocity = new Vector3(
+            Mathf.Clamp(m_rb.velocity.x, -max_speed, max_speed),
+            m_rb.velocity.y,
+            m_rb.velocity.z
+            );
+    }
 }
