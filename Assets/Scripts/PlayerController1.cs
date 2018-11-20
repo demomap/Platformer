f1c550490ecfdbf6270210b31b8f6e8bdb594805
Bryant_Jason <bryaj185@hsd.k12.or.us>
2018-11-20 10:53:39 -0800

    added jumping to the game

--- Assets/Scripts/PlayerController1.cs
+++ Assets/Scripts/PlayerController1.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class PlayerController1 : MonoBehaviour {
     private Rigidbody m_rb;
     public float speed = 10.0F;
-    public float max_speed = 4F;
-
-
+    public float max_speed = 12.0F;
+    float jump_height = 300.0F;
+    private Collider m_collider;
+    private float collide4r_radius = 0.0F;
+    public float gronded_epsilon = 0.05F;
+    public int user_layer_platform;
 	// Use this for initialization
 	void Start () {
         m_rb = GetComponent<Rigidbody>();
+        m_collider = GetComponent<Collider>();
+        collide4r_radius = m_collider.bounds.extents.y;
+
 	}
 
     // Update is called once per frame
@@ -28,5 +34,16 @@ public class PlayerController1 : MonoBehaviour {
             m_rb.velocity.y,
             m_rb.velocity.z
             );
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
+            m_rb.AddForce(0.0F, jump_height, 0.0F);
+    }
+    bool isGrounded()
+    {
+        int platform_layer = 1 << user_layer_platform;
+        return Physics.Raycast(
+            transform.position,
+            Vector3.down,
+            collide4r_radius + gronded_epsilon,
+            platform_layer);
     }
 }
